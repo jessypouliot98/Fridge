c852e886d4463d25696fcdb516fc522e062eaa2c
Jessy Pouliot <jessy.pouliot@renorun.ca>
2022-03-01 19:15:34 -0500

    repo - monorepo

--- apps/app/metro.config.js
+++ apps/app/metro.config.js
@@ -7,11 +7,17 @@
 
 const path = require('path');
 
-// https://dushyant37.medium.com/how-to-use-lerna-with-react-native-1eaa79b5d8ec
-const watchFolders = [
-  path.resolve(__dirname + '/..'), //Relative path to packages directory
-  path.resolve(__dirname + '/../../node_modules') //Relative path to packages directory
-];
+const getWatchFolders = () => {
+  // https://dushyant37.medium.com/how-to-use-lerna-with-react-native-1eaa79b5d8ec
+  const watchFolders = [
+    path.resolve(__dirname, '../../', 'packages'), //Relative path to packages directory
+    path.resolve(__dirname, '../../', 'node_modules') //Relative path to packages directory
+  ];
+
+  console.log({ watchFolders })
+
+  return watchFolders;
+}
 
 module.exports = {
   transformer: {
@@ -22,5 +28,5 @@ module.exports = {
       },
     }),
   },
-  watchFolders,
+  watchFolders: getWatchFolders(),
 };
